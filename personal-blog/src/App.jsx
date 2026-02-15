efd13c8b925f479d84078460a293ccca2840e584
Keegan Hinton <khinton386@gmail.com>
2026-02-15 00:41:07 -0500

    strated proper layout for multi-page react project

--- personal-blog/src/App.jsx
+++ personal-blog/src/App.jsx
@@ -1,30 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Blog from './pages/Blog';    
+import Projects from './pages/Projects';   
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Welcome to My Deep Dive Into Everything Tech</h1>
-      <body>
-        welcome to my personal blog. this is where i will be sharing my personal adventure with projects, studying, and homelabbing ups and downs!
-      </body>
-      <p className="read-the-docs">
-        Keegan Hinton, Khinton386@gmail.com, github.com/soibun-sol/
-      </p>
-    </>
-  )
+    <div className="content" style={{ minHeight: "100vh", width: "100vw" }}>
+    <Router>
+      < Header />
+      <main className= "style">
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      </main>
+      < Footer />
+    </Router>
+    </div>
+  );
 }
 
 export default App
